6c6349060dbdd350c2b275e742c42be5bb3eb165
satishparimi <satishparimi.qa@gmail.com>
2019-08-05 20:05:06 +0530

    updated web config file for css loaders

---
+++ src/Components/TaskManager/TaskManager.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './TaskManager.css';
+
+const TaskManager = () =>{
+
+    return(
+
+        <div className={classes.Task}>
+            <p>Taskmanager component</p>
+        </div>
+    );
+
+
+}
+
+export default TaskManager;
